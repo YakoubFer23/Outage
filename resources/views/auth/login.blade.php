a3be0853a640f6930431335c7c4209e0f1f54638
YakoubFer23 <yakoub_23@live.fr>
2023-01-31 19:34:40 +0100

    livrable

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Login - SB Admin</title>
+        <title>Waves Outage - Login</title>
         <link href="css/styles.css" rel="stylesheet" />
         <script src="assets/dist/all.js" crossorigin="anonymous"></script>
     </head>
--- resources/views/trash.blade.php
+++ resources/views/trash.blade.php
@@ -140,11 +140,7 @@
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
                             <div class="text-muted">Copyright &copy; Herbaia 2023</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
+                            
                         </div>
                     </div>
                 </footer>
